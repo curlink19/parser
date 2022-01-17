d56a5ed5ba8c1a4014943786098fec77c1977c68
curlink19 <p.kurochkin200@gmail.com>
2022-01-17 17:50:14 +0300

    status bar

--- util/get.sh
+++ util/get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-content=$(wget codeforces.com/contest/$1/problem/$2 -q -O -)
+content=$(wget --show-progress codeforces.com/contest/$1/problem/$2 -q -O -)
 echo $content > content.html
